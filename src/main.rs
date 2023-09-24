a962c5eddffc24064a1c2e069c902fa96b07e30a
Arthur Havlicek <arthurhavlicek@gmail.com>
2023-09-24 16:14:32 +0200

    rebase on main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{
     error::Error,
+    future::{self, Ready},
     net::Ipv4Addr,
 };
 
 use actix_web::{
+    dev::{Service, ServiceRequest, ServiceResponse, Transform},
     middleware::Logger,
     web::Data,
-    App, HttpServer,
+    App, HttpResponse, HttpServer,
 };
 
 
@@ -19,11 +21,59 @@ mod stores {
 
 use crate::store_interface::TodoStore;
 
+use futures::future::LocalBoxFuture;
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, SecurityScheme},
+    Modify, OpenApi,
+};
+use utoipa_rapidoc::RapiDoc;
+use utoipa_redoc::{Redoc, Servable};
+use utoipa_swagger_ui::SwaggerUi;
+
+use crate::schemas::{ErrorResponse, Todo, TodoUpdateRequest};
+
+const API_KEY_NAME: &str = "todo_apikey";
+const API_KEY: &str = "utoipa-rocks";
 
 #[actix_web::main]
 async fn main() -> Result<(), impl Error> {
     env_logger::init();
 
+
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(
+            rest::get_todos,
+            rest::create_todo,
+            rest::delete_todo,
+            rest::get_todo_by_id,
+            rest::update_todo,
+            rest::search_todos
+        ),
+        components(
+            schemas(Todo, TodoUpdateRequest, ErrorResponse)
+        ),
+        tags(
+            (name = "todo", description = "Todo management endpoints.")
+        ),
+        modifiers(&SecurityAddon)
+    )]
+    struct ApiDoc;
+
+    struct SecurityAddon;
+
+    impl Modify for SecurityAddon {
+        fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+            let components = openapi.components.as_mut().unwrap(); // we can unwrap safely since there already is components registered.
+            components.add_security_scheme(
+                "api_key",
+                SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("todo_apikey"))),
+            )
+        }
+    }
+
+    // Make instance variable of ApiDoc so all worker threads gets the same instance.
+    let openapi = ApiDoc::openapi();
     let data_store: Data<TodoStore> = Data::new(TodoStore::new());
 
     HttpServer::new(move || {
@@ -31,9 +81,141 @@ async fn main() -> Result<(), impl Error> {
         App::new()
             .wrap(Logger::default())
             .configure(rest::configure(data_store.clone()))
+            .service(Redoc::with_url("/redoc", openapi.clone()))
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", openapi.clone()),
+            )
+            // There is no need to create RapiDoc::with_openapi because the OpenApi is served
+            // via SwaggerUi instead we only make rapidoc to point to the existing doc.
+            .service(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+            // Alternative to above
+            // .service(RapiDoc::with_openapi("/api-docs/openapi2.json", openapi.clone()).path("/rapidoc"))
     })
     .bind((Ipv4Addr::UNSPECIFIED, 8080))?
     .run()
     .await
 }
 
+
+/// Require api key middleware will actually require valid api key
+struct RequireApiKey;
+
+impl<S> Transform<S, ServiceRequest> for RequireApiKey
+where
+    S: Service<
+        ServiceRequest,
+        Response = ServiceResponse<actix_web::body::BoxBody>,
+        Error = actix_web::Error,
+    >,
+    S::Future: 'static,
+{
+    type Response = ServiceResponse<actix_web::body::BoxBody>;
+    type Error = actix_web::Error;
+    type Transform = ApiKeyMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        future::ready(Ok(ApiKeyMiddleware {
+            service,
+            log_only: false,
+        }))
+    }
+}
+
+/// Log api key middleware only logs about missing or invalid api keys
+struct LogApiKey;
+
+impl<S> Transform<S, ServiceRequest> for LogApiKey
+where
+    S: Service<
+        ServiceRequest,
+        Response = ServiceResponse<actix_web::body::BoxBody>,
+        Error = actix_web::Error,
+    >,
+    S::Future: 'static,
+{
+    type Response = ServiceResponse<actix_web::body::BoxBody>;
+    type Error = actix_web::Error;
+    type Transform = ApiKeyMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        future::ready(Ok(ApiKeyMiddleware {
+            service,
+            log_only: true,
+        }))
+    }
+}
+
+struct ApiKeyMiddleware<S> {
+    service: S,
+    log_only: bool,
+}
+
+impl<S> Service<ServiceRequest> for ApiKeyMiddleware<S>
+where
+    S: Service<
+        ServiceRequest,
+        Response = ServiceResponse<actix_web::body::BoxBody>,
+        Error = actix_web::Error,
+    >,
+    S::Future: 'static,
+{
+    type Response = ServiceResponse<actix_web::body::BoxBody>;
+    type Error = actix_web::Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, actix_web::Error>>;
+
+    fn poll_ready(
+        &self,
+        ctx: &mut core::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(ctx)
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let response = |req: ServiceRequest, response: HttpResponse| -> Self::Future {
+            Box::pin(async { Ok(req.into_response(response)) })
+        };
+
+        match req.headers().get(API_KEY_NAME) {
+            Some(key) if key != API_KEY => {
+                if self.log_only {
+                    log::debug!("Incorrect api api provided!!!")
+                } else {
+                    return response(
+                        req,
+                        HttpResponse::Unauthorized().json(ErrorResponse::Unauthorized(
+                            String::from("incorrect api key"),
+                        )),
+                    );
+                }
+            }
+            None => {
+                if self.log_only {
+                    log::debug!("Missing api key!!!")
+                } else {
+                    return response(
+                        req,
+                        HttpResponse::Unauthorized()
+                            .json(ErrorResponse::Unauthorized(String::from("missing api key"))),
+                    );
+                }
+            }
+            _ => (), // just passthrough
+        }
+
+        if self.log_only {
+            log::debug!("Performing operation")
+        }
+
+        let future = self.service.call(req);
+
+        Box::pin(async move {
+            let response = future.await?;
+
+            Ok(response)
+        })
+    }
+}
--- src/rest.rs
+++ src/rest.rs
@@ -3,11 +3,11 @@ use actix_web::{
     web::{Data, Json, Path, Query, ServiceConfig},
     HttpResponse, Responder,
 };
-use serde::Deserialize;
-
 
 use crate::TodoStore;
 use crate::schemas::{ErrorResponse, TodoUpdateRequest, Todo};
+use serde::Deserialize;
+use utoipa::IntoParams;
 
 
 pub(super) fn configure(store: Data<TodoStore>) -> impl FnOnce(&mut ServiceConfig) {
@@ -23,7 +23,6 @@ pub(super) fn configure(store: Data<TodoStore>) -> impl FnOnce(&mut ServiceConfi
     }
 }
 
-
 #[get("/health")]
 async fn again() -> impl Responder {
     HttpResponse::Ok().body("")
@@ -37,9 +36,19 @@ async fn again() -> impl Responder {
 /// ```text
 /// curl localhost:8080/todo
 /// ```
+#[utoipa::path(
+    responses(
+        (status = 200, description = "List current todo items", body = [Todo])
+    )
+)]
 #[get("/todo")]
 pub(super) async fn get_todos(todo_store: Data<TodoStore>) -> impl Responder {
+<<<<<<< HEAD
     HttpResponse::Ok().json(todo_store.get_repository().read_all().await)
+=======
+    HttpResponse::Ok().json(todo_store.get_repository().read_all())
+
+>>>>>>> 2ad0d51 (rebase on main)
 }
 
 /// Create new Todo to storage.
@@ -51,13 +60,21 @@ pub(super) async fn get_todos(todo_store: Data<TodoStore>) -> impl Responder {
 /// ```text
 /// curl localhost:8080/todo -d '{"id": 1, "value": "Buy movie ticket", "checked": false}'
 /// ```
+#[utoipa::path(
+    request_body = Todo,
+    responses(
+        (status = 201, description = "Todo created successfully", body = Todo),
+        (status = 409, description = "Todo with id already exists", body = ErrorResponse, example = json!(ErrorResponse::Conflict(String::from("id = 1"))))
+    )
+)]
 #[post("/todo")]
 pub(super) async fn create_todo(todo: Json<Todo>, todo_store: Data<TodoStore>) -> impl Responder {
     let result = todo_store.get_repository().create_one(&todo.into_inner()).await;
     return match result {
-        Ok(todo_ret) => HttpResponse::Ok().json(todo_ret),
+        Ok(()) => HttpResponse::Ok().finish(),
         Err(existing) =>  HttpResponse::Conflict().json(ErrorResponse::Conflict(format!("id = {}", existing.id)))
     }
+
 }
 
 /// Delete Todo by given path variable id.
@@ -66,6 +83,19 @@ pub(super) async fn create_todo(todo: Json<Todo>, todo_store: Data<TodoStore>) -
 ///
 /// Api will delete todo from storage by the provided id and return success 200.
 /// If storage does not contain `Todo` with given id 404 not found will be returned.
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Todo deleted successfully"),
+        (status = 401, description = "Unauthorized to delete Todo", body = ErrorResponse, example = json!(ErrorResponse::Unauthorized(String::from("missing api key")))),
+        (status = 404, description = "Todo not found by id", body = ErrorResponse, example = json!(ErrorResponse::NotFound(String::from("id = 1"))))
+    ),
+    params(
+        ("id", description = "Unique storage id of Todo")
+    ),
+    security(
+        ("api_key" = [])
+    )
+)]
 #[delete("/todo/{id}")]
 pub(super) async fn delete_todo(id: Path<u64>, todo_store: Data<TodoStore>) -> impl Responder {
     let result = todo_store.get_repository().delete_one(*id).await;
@@ -78,6 +108,15 @@ pub(super) async fn delete_todo(id: Path<u64>, todo_store: Data<TodoStore>) -> i
 /// Get Todo by given todo id.
 ///
 /// Return found `Todo` with status 200 or 404 not found if `Todo` is not found from shared in-memory storage.
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Todo found from storage", body = Todo),
+        (status = 404, description = "Todo not found by id", body = ErrorResponse, example = json!(ErrorResponse::NotFound(String::from("id = 1"))))
+    ),
+    params(
+        ("id", description = "Unique storage id of Todo")
+    )
+)]
 #[get("/todo/{id}")]
 pub(super) async fn get_todo_by_id(id: Path<u64>, todo_store: Data<TodoStore>) -> impl Responder {
     let result = todo_store.get_repository().read_one(*id).await;
@@ -95,6 +134,20 @@ pub(super) async fn get_todo_by_id(id: Path<u64>, todo_store: Data<TodoStore>) -
 /// Tries to update `Todo` by given id as path variable. If todo is found by id values are
 /// updated according `TodoUpdateRequest` and updated `Todo` is returned with status 200.
 /// If todo is not found then 404 not found is returned.
+#[utoipa::path(
+    request_body = TodoUpdateRequest,
+    responses(
+        (status = 200, description = "Todo updated successfully", body = Todo),
+        (status = 404, description = "Todo not found by id", body = ErrorResponse, example = json!(ErrorResponse::NotFound(String::from("id = 1"))))
+    ),
+    params(
+        ("id", description = "Unique storage id of Todo")
+    ),
+    security(
+        (),
+        ("api_key" = [])
+    )
+)]
 #[put("/todo/{id}")]
 pub(super) async fn update_todo(
     id: Path<u64>,
@@ -109,7 +162,7 @@ pub(super) async fn update_todo(
 }
 
 /// Search todos Query
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, IntoParams)]
 pub(super) struct SearchTodos {
     /// Content that should be found from Todo's value field
     value: String,
@@ -119,6 +172,15 @@ pub(super) struct SearchTodos {
 ///
 /// Perform search from `Todo`s present in in-memory storage by matching Todo's value to
 /// value provided as query parameter. Returns 200 and matching `Todo` items.
+
+#[utoipa::path(
+    params(
+        SearchTodos
+    ),
+    responses(
+        (status = 200, description = "Search Todos did not result error", body = [Todo]),
+    )
+)]
 #[get("/todo/search")]
 pub(super) async fn search_todos(
     query: Query<SearchTodos>,
